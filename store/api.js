df72189a477de1a37ab9d69dbd9b56aca6e2866e
athibau2 <a.thibs98@gmail.com>
2023-05-08 02:37:46 -0600

    added ssl cert on ec2 for api subdomain

--- store/api.js
+++ store/api.js
@@ -1,2 +1,3 @@
-export const API = "http://ec2-18-144-43-167.us-west-1.compute.amazonaws.com:3001"
+// export const API = "http://ec2-18-144-43-167.us-west-1.compute.amazonaws.com:3001"
 // export const API = "localhost:3001/api"
+export const API = "https://api.joinwritenow.com"
