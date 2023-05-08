6331f86fbc8d84d5a2cea563eb7af44fb3da99e6
athibau2 <a.thibs98@gmail.com>
2023-05-08 01:28:58 -0600

    uncommented proxy code

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,17 +55,17 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'http://ec2-18-144-43-167.us-west-1.compute.amazonaws.com:3001',
-    // proxy: true
+    proxy: true
   },
 
-  // proxy: {
-  //   '/api/': {
-  //     target: 'http://ec2-18-144-43-167.us-west-1.compute.amazonaws.com:3001',
-  //     pathRewrite: {
-  //       '^/api/': '/'
-  //     }
-  //   }
-  // },
+  proxy: {
+    '/api/': {
+      target: 'http://ec2-18-144-43-167.us-west-1.compute.amazonaws.com:3001',
+      pathRewrite: {
+        '^/api/': '/'
+      }
+    }
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
